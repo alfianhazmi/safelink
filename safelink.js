849c272089fa8c915cfbd72c31d75bdc8a37ba13
alfianhazmi <85866872+alfianhazmi@users.noreply.github.com>
2021-08-09 14:43:10 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*15)+ 1);
+var x=Math.floor((Math.random()*16)+ 1);
 var xxx=null;
 if(x=="1"){xxx="criteria-for-trusted-online-money-loans.html"}
 if(x=="2"){xxx="even-asleep-jeff-bezos-can-earn-usd.html"}
@@ -35,7 +35,8 @@ if(x=="11"){xxx="6-features-of-good-4k-portable-monitor.html"}
 if(x=="12"){xxx="5-ways-to-extend-your-android.html"}
 if(x=="13"){xxx="armored-landcruiser-200-series.html"}
 if(x=="14"){xxx="how-to-start-your-online-travel-business.html"}
-if(x=="15"){xxx="complications-about-tourist-visa.html"}a_to_ve[i].href="https://direct.mah-channel.my.id/2021/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="15"){xxx="complications-about-tourist-visa.html"}
+if(x=="16"){xxx="how-to-go-about-choosing-web-designing.html"}a_to_ve[i].href="https://direct.mah-channel.my.id/2021/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
