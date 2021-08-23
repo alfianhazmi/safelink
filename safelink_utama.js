e1a70502626bc0c7d6b370051fa510a77afc1a29
alfianhazmi <85866872+alfianhazmi@users.noreply.github.com>
2021-08-23 08:00:32 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -45,7 +45,7 @@ if(x=="21"){xxx="youre-paying-almost-40-more-for-your.html"}
 if(x=="22"){xxx="why-business-owners-seek-to-enter.html"}
 if(x=="23"){xxx="the-key-trends-in-global-confectionery.html"}
 if(x=="24"){xxx="astaxanthin-market-exhibiting-high.html"}
-if(x=="25"){xxx="ed-gold-apple.html"}a_to_ve[i].href="https://direct.mah-channel.my.id/2021/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="25"){xxx="red-gold-apple.html"}a_to_ve[i].href="https://direct.mah-channel.my.id/2021/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
